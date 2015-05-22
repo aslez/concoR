70251f4afe20b90e0d46f5bb32b2610831cc6115
aslez <aslez@virginia.edu>
2015-05-21 21:50:01 -0400

    Updated code to pass block information to the blockmodel command in sna.  Dropped block_dens and renamed the block command.

--- R/concoR.R
+++ R/concoR.R
@@ -5,11 +5,14 @@
 
 #'@param m0 A list of \eqn{n} x \eqn{n} matrices, each of which refers to a
 #'relation on a set of vertices.  Only one-mode data are supported at present.
-#'#'@param cutoff A value between 0 and 1 used to determine convergence.
+#'@param cutoff A value between 0 and 1 used to determine convergence.
 #'@param max.iter An integer representing the maximum number of iterations.
 #'@param p An integer representing the desired number of partitions.
 
 #'@return A \code{data.frame} depicting the block assignment for each vertex.
+#'Rows are sorted to match the order in which vertices appear in the original
+#'data.  The column vector indicating block membership can be passed directly to
+#' the \code{blockmodel} command in \code{sna}. 
 #'
 #'@references Breiger, R.L., Boorman, S.A., and Arabie, P.  1975.  An Algorithm
 #'for Clustering Relational Data with Applications to Social Network Analysis
@@ -18,10 +21,13 @@
 #'
 #'@examples
 #'data(bank_wiring)
-#'b <- block(bank_wiring, p = 2)
-#'lapply(bank_wiring, block_dens, blocks = b)
+#'library(sna)
+#'b <- concor_hca(bank_wiring, p = 2)
+#'g <- as.sociomatrix.sna(bank_wiring)
+#'bm <- blockmodel(g, ec = b$block, glabels = names(bank_wiring))
+#'plot(bm)
 
-block <- function(m0, cutoff = 0.999, max.iter = 25, p = 1) {
+concor_hca <- function(m0, cutoff = 0.999, max.iter = 25, p = 1) {
   mat_stack <- do.call(rbind, m0)
   p_list <- list(mat_stack)
   for(i in 1:p) {
@@ -29,10 +35,11 @@ block <- function(m0, cutoff = 0.999, max.iter = 25, p = 1) {
                             function(x) concor(x, cutoff, max.iter)), 
                      recursive = FALSE)
   }
-  do.call(rbind, block_names(p_list))
+  df <- do.call(rbind, block_names(p_list))
+  df[match(rownames(m0[[1]]), df$vertex), ]
 }
 
-#'@rdname block
+#'@rdname concor_hca
 concor <- function(m0, cutoff = 0.999, max.iter = 50) {
   if (ncol(m0) < 2) stop("Too few columns to partition.")
   mi <- cor(m0)
@@ -45,7 +52,7 @@ concor <- function(m0, cutoff = 0.999, max.iter = 50) {
   list(m0[, group, drop = FALSE], m0[, !group, drop = FALSE])
 }
 
-#'@rdname block
+#'@rdname concor_hca
 block_names <- function(p_list) {
   lapply(seq_along(p_list), 
          function(x) data.frame(block = x, 
